6caf0d5e275df6d67941d96e24402959ee6b826e
alexPozdnyak <ya.pozdnyak@gmail.com>
2025-09-05 14:53:28 +0400

    chenge 2

--- script/JS/script.js
+++ script/JS/script.js
@@ -1,5 +1,59 @@
 "use strict";
-console.log('1');
-console.log('5');
-console.log(500);
+// Class Test
+class T_01 {
+    name = '';
+}
+class T_02 extends T_01 {
+    email = '';
+}
+class T_03 extends T_02 {
+    getData() {
+        console.log('qqqq', this.name);
+        console.log('qqqq', this.email);
+    }
+}
+class T_04 extends T_01 {
+    email = '';
+    constructor(name, email) {
+        super();
+        this.name = name;
+        this.email = email;
+    }
+    getData() {
+        console.log('qqqq2', this.name);
+        console.log('qqqq3', this.email);
+    }
+    getData2() {
+        return {
+            name: this.name,
+            email: this.email,
+        };
+    }
+}
+class T_05 extends T_04 {
+    age = 0;
+    constructor(name, email, age) {
+        super(name, email);
+        this.age = age;
+    }
+    // Перезатереть метод полностью
+    /*getData(): void {
+      console.log(this.name, this.email, this.age);
+    }*/
+    // Дернем текущий метод  обращение к классу родителя через Super
+    getData() {
+        super.getData2();
+        console.log(this.age);
+    }
+    getData2() {
+        return {
+            name: super.getData2().name,
+            email: super.getData2().email,
+            age: this.age,
+        };
+    }
+}
+//Constructor
+const obj1 = new T_05('alexPozdnyak', 'ya.pozdnyak@yandex.ru', 35);
+console.log(obj1.getData2());
 //# sourceMappingURL=script.js.map
--- script/script.ts
+++ script/script.ts
@@ -1,3 +1,68 @@
-console.log('1');
-console.log('5');
-console.log(500);
+interface User {
+  name: string;
+  email: string;
+  age?: number;
+}
+// Class Test
+class T_01 {
+  name: string = '';
+}
+
+class T_02 extends T_01 {
+  email: string = '';
+}
+
+class T_03 extends T_02 {
+  getData(): void {
+    console.log('qqqq', this.name);
+    console.log('qqqq', this.email);
+  }
+}
+
+class T_04 extends T_01 {
+  email: string = '';
+  constructor(name: string, email: string) {
+    super();
+    this.name = name;
+    this.email = email;
+  }
+  getData(): void {
+    console.log('qqqq2', this.name);
+    console.log('qqqq3', this.email);
+  }
+
+  getData2(): User {
+    return {
+      name: this.name,
+      email: this.email,
+    };
+  }
+}
+
+class T_05 extends T_04 {
+  age: number = 0;
+  constructor(name: string, email: string, age: number) {
+    super(name, email);
+    this.age = age;
+  }
+  // Перезатереть метод полностью
+  /*getData(): void {
+    console.log(this.name, this.email, this.age);
+  }*/
+  // Дернем текущий метод  обращение к классу родителя через Super
+  getData(): void {
+    super.getData2();
+    console.log(this.age);
+  }
+  getData2(): User {
+    return {
+      name: super.getData2().name,
+      email: super.getData2().email,
+      age: this.age,
+    };
+  }
+}
+//Constructor
+
+const obj1 = new T_05('alexPozdnyak', 'ya.pozdnyak@yandex.ru', 35);
+console.log(obj1.getData2());
